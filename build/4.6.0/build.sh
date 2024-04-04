5f72c16f9e4f0a95661a9babc2c469d80d2d1d96
Mark Haslinghuis <mark@numloq.nl>
2024-04-04 21:26:52 +0200

    Add 4.5.0 and 4.6.0-dev

---
+++ build/4.6.0/build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+COMMIT=$1
+TARGET=$2
+FLAGS=$3
+
+echo ""
+echo "Pulling from Github (${COMMIT}):"
+
+cd /source
+git fetch origin "${COMMIT}":CLOUD_HEAD || { echo "fetch failed with $?"; exit 1; }
+[ $? == 1 ] && exit 0;
+
+echo ""
+echo "Checking out the branch -> CLOUD_HEAD (for ${COMMIT})"
+
+cd /source
+git checkout -f CLOUD_HEAD || { echo "checkout failed with $?"; exit 1; }
+[ $? == 1 ] && exit 0;
+
+echo ""
+echo "Commencing the build (make ${TARGET}):"
+
+cd /source
+make configs || { echo "make configs failed with $?"; exit 1; }
+[ $? == 1 ] && exit 0;
+
+cd /source
+make $TARGET EXTRA_FLAGS="${FLAGS}" || { echo "make failed with $?"; exit 1; }
+[ $? == 1 ] && exit 0;
+
+echo ""
+
+if [[ -d "/output" ]] ; then
+    echo "Copying hex files (to output):"
+    cp -v /source/obj/*.hex /output
+    echo " -> completed"
+else
+    echo "Skipping copying hex files (no output mount for container)"
+fi
